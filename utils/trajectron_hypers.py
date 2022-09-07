f00757df062a36b36b7fc12d97312178f384680d
enricobu96 <enrico.buratto.1@studenti.unipd.it>
2022-09-07 10:01:35 +0200

    experiment 4

--- utils/trajectron_hypers.py
+++ utils/trajectron_hypers.py
@@ -1,9 +1,9 @@
 def get_traj_hypers():
     hypers = {   'batch_size': 256,
-    'grad_clip': 1.0,
+    'grad_clip': 1.0, # clips gradient to 1
     'learning_rate_style': 'exp',
     'min_learning_rate': 1e-05,
-    'learning_decay_rate': 0.9999,
+    'learning_decay_rate': 0.99999,
     'prediction_horizon': 12,
     'minimum_history_length': 1,
     'maximum_history_length': 7,
