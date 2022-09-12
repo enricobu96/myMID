3f75fe8385d9570460d57bce9d1a15bd00210264
enricobu96 <enrico.buratto.1@studenti.unipd.it>
2022-09-12 12:13:57 +0200

    works with cuda

--- models/diffusion.py
+++ models/diffusion.py
@@ -283,12 +283,12 @@ class DiffusionTraj(Module):
             t = self.var_sched.uniform_sample_t(batch_size)
 
         alpha_bar = self.var_sched.alpha_bars[t]
-        beta = self.var_sched.betas[t].to('cpu')
+        beta = self.var_sched.betas[t].cuda()
 
-        c0 = torch.sqrt(alpha_bar).view(-1, 1, 1).to('cpu')     # (B, 1, 1)
-        c1 = torch.sqrt(1 - alpha_bar).view(-1, 1, 1).to('cpu')   # (B, 1, 1)
+        c0 = torch.sqrt(alpha_bar).view(-1, 1, 1).cuda()    # (B, 1, 1)
+        c1 = torch.sqrt(1 - alpha_bar).view(-1, 1, 1).cuda()  # (B, 1, 1)
 
-        e_rand = torch.randn_like(x_0).to('cpu')  # (B, N, d)
+        e_rand = torch.randn_like(x_0).cuda()  # (B, N, d)
 
 
         e_theta = self.net(c0 * x_0 + c1 * e_rand, beta=beta, context=context)
